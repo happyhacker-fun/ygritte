10dc213e1c5b185a0ed27555c67d7b705ef554bb
Frost Wong <frostwong@gmail.com>
2017-07-15 20:36:59 +0800

    split request and response

--- src/Ygritte/ContentType.php
+++ src/Ygritte/ContentType.php
@@ -9,18 +9,67 @@
 namespace Ygritte;
 
 
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class ContentType
 {
-    private static $readable = [
+    private static $readableRequest = [
+        'application/x-www-form-urlencoded',
         'application/json',
+        'application/javascript',
         'application/xml',
         'text/plain',
         'text/xml',
         'text/html',
+        'text',
     ];
 
-    public static function isReadable($contentType)
+    private static $readableResponse = [
+        'text/plain',
+        'text/xml',
+        'text/html',
+        'application/json',
+        'application/javascript',
+        'application/xml',
+        'text/plain',
+        'text/xml',
+        'text/html',
+        'text',
+    ];
+
+    public static function isRequestReadable(RequestInterface $request)
     {
-        return in_array($contentType, self::$readable);
+        $contentType = self::getContentType($request);
+
+        return in_array($contentType, self::$readableRequest);
+    }
+
+    private static function getContentType(MessageInterface $message)
+    {
+        $contentTypeHeader = $message->getHeader('Content-Type');
+        if ($contentTypeHeader) {
+            return $contentTypeHeader[0];
+        }
+
+        return 'text';
+    }
+
+    public static function isResponseReadable(ResponseInterface $response)
+    {
+        $contentType = self::getContentType($response);
+
+        return in_array($contentType, self::$readableResponse);
+    }
+
+    public static function getReadableRequest()
+    {
+        return self::$readableRequest;
+    }
+
+    public static function getReadableResponse()
+    {
+        return self::$readableResponse;
     }
 }
